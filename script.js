645cd1d8f668c0407d173d5a2ba48c6581de9858
Sena <35301617+senacanik@users.noreply.github.com>
2024-09-24 14:21:42 +0300

    feat: Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const style = document.createElement('style');
+style.innerHTML = 
+    .red-bg {
+        background-color: red !important; 
+    }
+;
+document.head.appendChild(style);
+setTimeout(() => {
+    document.querySelectorAll('a').forEach(function(a) {
+        const hrefValue = a.getAttribute('href');
+        if (!hrefValue || hrefValue === 'javascript:;' || hrefValue === '#' || hrefValue === "") {
+            a.classList.add('red-bg');
+        }
+    });
+}, 1000); 
